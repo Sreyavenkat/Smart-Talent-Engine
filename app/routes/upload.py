fce22e9988e02741d8536595a56c0f08940a68c5
Sreya V <sreyavenkat80@gmail.com>
2026-04-01 10:11:07 +0530

    similarity

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -4,11 +4,14 @@ from typing import List
 from app.utils.file_handler import save_file
 from app.services.parser import extract_text_from_pdf
 from app.services.embedding import get_embedding
+from app.utils.similarity import cosine_similarity
 
 router = APIRouter()
 
 # temporary in-memory storage (we improve later)
 DATABASE = []
+
+JD_DATA = {}
 
 @router.post("/upload-resumes")
 async def upload_resumes(files: List[UploadFile] = File(...)):
@@ -41,4 +44,43 @@ async def upload_resumes(files: List[UploadFile] = File(...)):
     return {
         "message": "Resumes processed successfully",
         "files": results
+    }
+
+@router.post("/upload-jd")
+async def upload_jd(jd: str):
+    from app.services.embedding import get_embedding
+
+    embedding = get_embedding(jd)
+
+    JD_DATA["text"] = jd
+    JD_DATA["embedding"] = embedding.tolist()
+
+    return {
+        "message": "Job Description processed successfully"
+    }
+
+
+
+@router.get("/rank-candidates")
+def rank_candidates():
+    if "embedding" not in JD_DATA:
+        return {"error": "Upload JD first"}
+
+    jd_embedding = JD_DATA["embedding"]
+
+    ranked = []
+
+    for candidate in DATABASE:
+        score = cosine_similarity(candidate["embedding"], jd_embedding)
+
+        ranked.append({
+            "filename": candidate["filename"],
+            "score": round(float(score) * 100, 2)
+        })
+
+    # sort descending
+    ranked = sorted(ranked, key=lambda x: x["score"], reverse=True)
+
+    return {
+        "ranked_candidates": ranked
     }
